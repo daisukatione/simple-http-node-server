21227777bd3a894eae4621c344c441eca3322794
郷大助 <daisukegoh@guest1-47.csl.sony.co.jp>
2019-07-02 13:29:47 +0900

    20190702

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const http = require('http');
 const port = 3000;
+const fs = require('fs');
 
 // Handles HTTP requests.
 const requestHandler = (request, response) => {
   response.end(`Handling a request on port ${port}`)
 };
 
+fs.writeFileSync('hello-world.txt', 'Hello to this great world', 'utf8');
+
 // Create a server and pass in the  requestHandler function
 const server = http.createServer(requestHandler);
 
-// Start the server listening on port 3000
+// Start the server listening on port 8000
 server.listen(port, (err) => {
   if (err) {
     return console.log(`You have an error:  ${err}`);
@@ -18,8 +21,3 @@ server.listen(port, (err) => {
   console.log(`server is listening on ${port}`);
 });
 
-fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
-  if (err) throw err;
-
-  console.log('success')
-});
